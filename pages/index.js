6f5b706950841d44f3129f9e2aba3a49f8239015
ayushsaranGithuB <112019897+ayushsaranGithuB@users.noreply.github.com>
2023-05-06 02:50:45 +0530

    typos

--- pages/index.js
+++ pages/index.js
@@ -37,13 +37,13 @@ export default function App() {
             <h4>
             <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512"><path d="M416 32H31.9C14.3 32 0 46.5 0 64.3v383.4C0 465.5 14.3 480 31.9 480H416c17.6 0 32-14.5 32-32.3V64.3c0-17.8-14.4-32.3-32-32.3zM135.4 416H69V202.2h66.5V416zm-33.2-243c-21.3 0-38.5-17.3-38.5-38.5S80.9 96 102.2 96c21.2 0 38.5 17.3 38.5 38.5 0 21.3-17.2 38.5-38.5 38.5zm282.1 243h-66.4V312c0-24.8-.5-56.7-34.5-56.7-34.6 0-39.9 27-39.9 54.9V416h-66.4V202.2h63.7v29.2h.9c8.9-16.8 30.6-34.5 62.9-34.5 67.2 0 79.7 44.3 79.7 101.9V416z"/></svg>
               <a href="https://www.linkedin.com/in/ayushsaran-designer/">https://www.linkedin.com/in/ayushsaran-designer/</a> | OR check out my <a href="/cv">Resume</a>[pdf]</h4>
-            <h3 class="sandbox">
+            <h3 className="sandbox">
               Sandbox
             </h3>
             <h4>
               <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 640 512"><path d="M392.8 1.2c-17-4.9-34.7 5-39.6 22l-128 448c-4.9 17 5 34.7 22 39.6s34.7-5 39.6-22l128-448c4.9-17-5-34.7-22-39.6zm80.6 120.1c-12.5 12.5-12.5 32.8 0 45.3L562.7 256l-89.4 89.4c-12.5 12.5-12.5 32.8 0 45.3s32.8 12.5 45.3 0l112-112c12.5-12.5 12.5-32.8 0-45.3l-112-112c-12.5-12.5-32.8-12.5-45.3 0zm-306.7 0c-12.5-12.5-32.8-12.5-45.3 0l-112 112c-12.5 12.5-12.5 32.8 0 45.3l112 112c12.5 12.5 32.8 12.5 45.3 0s12.5-32.8 0-45.3L77.3 256l89.4-89.4c12.5-12.5 12.5-32.8 0-45.3z"/></svg>
               <a href="https://sandbox.ayushsaran.info">/sandbox</a> - Recent Dev Experiments</h4>
-            <h3 class="sandbox">
+            <h3 className="sandbox">
               Personal Blog
             </h3>
             <h4>
--- pages/sandbox.jsx
+++ pages/sandbox.jsx
@@ -10,20 +10,20 @@ const SandBox = () => {
  
     </Head>
         <div className="sandboxPage">
-            	<section class="sectionPageintro">
+            	<section className="sectionPageintro">
 		<h1>Development<br /><b>Sandbox</b></h1>
 	</section>
-	<section class="pageSection">
+	<section className="pageSection">
 		<h1>This page has moved...</h1>
 		<h2>See: https://sandbox.ayushsaran.info/</h2>
         
-		<div class="project">
-			<div class="info">
+		<div className="project">
+			<div className="info">
 				<h2>Personal Blog</h2>
 				<h3>A NetlifyCMS powered static blog site generated with 11ty</h3>
 				<p>A JAM-Stack Static Blog generated server-side using <a href="https://www.11ty.dev/" target="_blank">11ty.js</a> with content stored in git and accessed via the Headless <a href="https://decapcms.org/" target="_blank">DecapCMS</a></p>
 			</div>
-			<ul class="techStack">
+			<ul className="techStack">
 				<li>
 					<h4>Tech Stack:</h4>
 				</li>
@@ -32,18 +32,18 @@ const SandBox = () => {
 				<li>SSG: <a href='https://www.11ty.dev/' target="_blank">11ty.js</a></li>
 				<li>Hosting & CD: <a href='https://netlify.com/' target="_blank">Netlify</a></li>
 			</ul>
-			<a href="http://blog.ayushsaran.info" target="_blank" class="projectLink">blog.ayushsaran.info</a>
+			<a href="http://blog.ayushsaran.info" target="_blank" className="projectLink">blog.ayushsaran.info</a>
 		</div>
 		
-		<div class="project">
-			<div class="info">
+		<div className="project">
+			<div className="info">
 				<h2>devNotes</h2>
 				<h3>A Markdown Blog powered by Next.Js</h3>
 				<p>A Static Blog Website generated using Next.Js with content stored in Markdown
 					and Rendered via the <a href="https://www.npmjs.com/package/gray-matter" target="_blank">gray-matter</a>
 					and <a href="https://www.npmjs.com/package/remarkable" target="_blank">reMarkable</a> libraries</p>
 			</div>
-			<ul class="techStack">
+			<ul className="techStack">
 				<li>
 					<h4>Tech Stack:</h4>
 				</li>
@@ -52,18 +52,18 @@ const SandBox = () => {
 				<li>Routing: <a href='https://nextjs.org/' target="_blank">Next.js</a></li>
 				<li>Hosting & CD: <a href='https://vercel.com/' target="_blank">Vercel</a></li>
 			</ul>
-			<a href="http://devnotes.ayushsaran.info" target="_blank" class="projectLink">devnotes.ayushsaran.info</a>
+			<a href="http://devnotes.ayushsaran.info" target="_blank" className="projectLink">devnotes.ayushsaran.info</a>
 		</div>
 		
-		<div class="project">
-			<div class="info">
+		<div className="project">
+			<div className="info">
 				<h2>Ayush Recommends</h2>
 				<h3>A curated Movie/TV Show recommendation site</h3>
 				<p>
 					MERN stack Website
 				</p>
 			</div>
-			<ul class="techStack">
+			<ul className="techStack">
 				<li>
 					<h4>Tech Stack:</h4>
 				</li>
@@ -73,7 +73,7 @@ const SandBox = () => {
 				<li>Routing: <a href='https://nextjs.org/' target="_blank">Express.js</a></li>
 				<li>Hosting & CD: <a href='https://railway.app/' target="_blank">Railway</a></li>
 			</ul>
-			<a href="http://recommendations.ayushsaran.info" target="_blank" class="projectLink">recommendations.ayushsaran.info</a>
+			<a href="http://recommendations.ayushsaran.info" target="_blank" className="projectLink">recommendations.ayushsaran.info</a>
 		</div>
 		{/* <!--/project--> */}
 
