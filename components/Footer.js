13108f37da2ef041eb0354f652f96ee1286c4845
ayushsaranGithuB <112019897+ayushsaranGithuB@users.noreply.github.com>
2023-05-12 00:25:10 +0530

    URLS

--- components/Footer.js
+++ components/Footer.js
@@ -4,9 +4,9 @@ const Footer = () => {
         <footer>
             <div class="footerWrapper">
                 <section class="socialIcons">
-                    <a href="/behance"><img src='/img/btn-be.png' id="btn-be" /></a>
-                    <a href="/behance"><img src='/img/dribble.png' id="btn-drbl"/></a>
-                    <a href="/behance"><img src='/img/linkedIn.png'  id="btn-li" /></a>
+                    <a href="https://www.behance.net/ayushsaran"><img src='/img/btn-be.png' id="btn-be" /></a>
+                    <a href="https://dribbble.com/AyushSaran"><img src='/img/dribble.png' id="btn-drbl"/></a>
+                    <a href="https://www.linkedin.com/in/ayushsaran-designer/"><img src='/img/linkedIn.png'  id="btn-li" /></a>
                 </section>
                 
                 <section class="contact">
